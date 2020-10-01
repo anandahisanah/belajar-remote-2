482bcfba7ad9966fca47426d409c1b390205b4c9
anandahisanah <nurhisanah@gmail.com>
2020-10-01 15:26:47 +0800

    menambah di index.php

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 </head>
 <body>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa exercitationem magni illum, explicabo earum tempora dolorum quos. Aspernatur incidunt provident debitis eveniet recusandae corrupti dolore blanditiis. Quo nihil perferendis natus?</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
